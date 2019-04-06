01be4e31ce68c63876ed64b915c252b940b259b6
REAL-BS\student12 <kitaev-mitynka@mail.ru>
2019-04-06 18:44:17 +0400

    Мой первый комит

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int a = 3;
+        int b = 4;
+        System.out.println("");
+
+    }
+}
